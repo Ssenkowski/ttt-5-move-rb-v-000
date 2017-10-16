09fe8cf6f9b4721918b61e44338950ae0275af17
Ssenkowski <scsenkowski@gmail.com>
2017-10-16 12:28:29 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+puts "Where would you like to go?"
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+move(board, input, token)
+
+def move(board, input, token = "X")
+board[input] = token
+end
+
+def input_to_index(input)
+  input = "1-9" - 1
+  "5".to_i
+end
+
+
 # code your input_to_index and move method here!
